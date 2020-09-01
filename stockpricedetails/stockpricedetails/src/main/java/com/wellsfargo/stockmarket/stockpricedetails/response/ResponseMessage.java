0c4c1d600c38a99b0635377f8a19e3ea3ccad8cb
Appy-123 <34472128+Appy-123@users.noreply.github.com>
2020-09-01 17:32:21 +0530

    Add files via upload

---
+++ stockpricedetails/stockpricedetails/src/main/java/com/wellsfargo/stockmarket/stockpricedetails/response/ResponseMessage.java
@@ -0,0 +1,14 @@
+package com.wellsfargo.stockmarket.stockpricedetails.response;
+
+public class ResponseMessage {
+
+	private String message;
+
+	  public ResponseMessage(String message) {
+	    this.message = message;
+	  }
+
+	  public String getMessage() {
+	    return message;
+	  }
+}
