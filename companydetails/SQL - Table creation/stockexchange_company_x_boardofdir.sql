5f7198d381211a1b0294c2b082c442a69426d29b
Shubham Garg <39940156+shubhgsgarg@users.noreply.github.com>
2020-09-01 14:30:50 +0530

    Update stockexchange_company_x_boardofdir.sql

--- companydetails/SQL - Table creation/stockexchange_company_x_boardofdir.sql
+++ companydetails/SQL - Table creation/stockexchange_company_x_boardofdir.sql
@@ -4,4 +4,4 @@ DROP TABLE IF EXISTS `company_x_boardofdir`;
 CREATE TABLE `company_x_boardofdir` (
   `AssocCompanyId` decimal(10,0) DEFAULT NULL,
   `boardOfDirName` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
