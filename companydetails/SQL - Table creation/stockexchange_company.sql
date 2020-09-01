97d891124e8e0c7b06fd87eb382f1ad4a338c727
Shubham Garg <39940156+shubhgsgarg@users.noreply.github.com>
2020-09-01 14:31:30 +0530

    Update stockexchange_company.sql

--- companydetails/SQL - Table creation/stockexchange_company.sql
+++ companydetails/SQL - Table creation/stockexchange_company.sql
@@ -9,4 +9,4 @@ CREATE TABLE `company` (
   `companyCode` decimal(10,0) DEFAULT NULL,
   `companyDetails` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`companyId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
