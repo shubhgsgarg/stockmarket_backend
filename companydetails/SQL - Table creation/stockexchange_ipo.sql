a92241ec50c95b049a67860a72baa30e83144027
Shubham Garg <39940156+shubhgsgarg@users.noreply.github.com>
2020-09-01 14:29:26 +0530

    Update stockexchange_ipo.sql

--- companydetails/SQL - Table creation/stockexchange_ipo.sql
+++ companydetails/SQL - Table creation/stockexchange_ipo.sql
@@ -1,7 +1,6 @@
 
 DROP TABLE IF EXISTS `ipo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `ipo` (
   `ipoId` int(11) NOT NULL AUTO_INCREMENT,
   `companyName` varchar(20) DEFAULT NULL,
@@ -11,4 +10,4 @@ CREATE TABLE `ipo` (
   `openDateTime` datetime DEFAULT NULL,
   `remarks` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ipoId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
