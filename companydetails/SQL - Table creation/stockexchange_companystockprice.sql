c1ccb2aadcad61124f62a1014fec24b5a4437c0d
Shubham Garg <39940156+shubhgsgarg@users.noreply.github.com>
2020-09-01 14:29:58 +0530

    Update stockexchange_companystockprice.sql

--- companydetails/SQL - Table creation/stockexchange_companystockprice.sql
+++ companydetails/SQL - Table creation/stockexchange_companystockprice.sql
@@ -9,4 +9,4 @@ CREATE TABLE `companystockprice` (
   `stockPriceDate` date DEFAULT NULL,
   `stockPriceTime` time DEFAULT NULL,
   PRIMARY KEY (`companyStockPriceId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
